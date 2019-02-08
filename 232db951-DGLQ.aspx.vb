c9e7ff19f91ea248b261a6695acc914cd751b208
ecacerest <ecacerest@finagil.com.mx>
2019-02-08 12:27:18 -0600

    ya se activo alta de lineas liquidez

--- 232db951-DGLQ.aspx.vb
+++ 232db951-DGLQ.aspx.vb
@@ -42,7 +42,7 @@ Partial Public Class DGSucursalLQForm
         Dim Analista As String = DetailsView1.Rows(9).Cells(1).Text.Trim
         Dim ta As New ProDSTableAdapters.SolLiqTableAdapter
         ta.UpdateEstatus("APROBADO", Request("User"), Request("ID"))
-        'Globales.AltaLineaCreditoLIQUIDEZ(Cliente, Monto, "Autorizado por " & Request("User"))
+        Globales.AltaLineaCreditoLIQUIDEZ(Cliente, Monto, "Autorizado por " & Request("User"))
         GeneraCorreoAUT(Monto, Cliente, Nombre, Analista)
         Response.Redirect("~\232db951-DGLQ.aspx?User=" & Request("User") & "&ID=0")
     End Sub
