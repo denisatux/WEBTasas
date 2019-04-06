b30214e0bb65dc9d1df39189164ef64f8af9e447
ecacerest <ecacerest@finagil.com.mx>
2019-04-05 19:16:10 -0600

    ASPX para correo de nueva web de finagil

---
+++ contact-form.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="contact-form.aspx.vb" Inherits="WEBTasas.contact_form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Names="Arial" ForeColor="#FF6600" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ contact-form.aspx.vb
@@ -0,0 +1,20 @@
+﻿Public Class contact_form
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Request.Form.Count > 0 Then
+            Dim Mensaje As String = ""
+
+            Mensaje = "Nombre: " & Request.Form.Item("name").ToUpper
+            Mensaje += "<br>Correo: " & Request.Form.Item("email").ToUpper
+            Mensaje += "<br>Mensaje: " & Request.Form.Item("message").ToUpper
+
+            MandaCorreoFase("WebFinagil@finagil.com.mx", "sistemas", Request.Form.Item("subject"), Mensaje)
+            Label1.Text = ("Gracias " & Request.Form.Item("name").ToUpper & " tu mensaje ha sido enviado. En breve te responderemos.")
+        Else
+
+            Label1.Text = ("Error de Envio")
+        End If
+    End Sub
+
+End Class
